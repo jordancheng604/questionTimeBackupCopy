df7a12360305bac6ba29499a7d4bf1401ae32a33
Jordan Cheng <51500782+jordancheng604@users.noreply.github.com>
2020-09-15 17:04:26 -0700

    Build folder for prod.

---
+++ build/precache-manifest.2da9da26e8ef00bb6deef41f70456cf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05af787834aa14f55bb5bb2de550a611",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7695738e2aaf3cb53d28",
+    "url": "/static/css/main.3605da21.chunk.css"
+  },
+  {
+    "revision": "3b6064be216a661b58ae",
+    "url": "/static/js/2.b6d1b9f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b6d1b9f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7695738e2aaf3cb53d28",
+    "url": "/static/js/main.ec054dd6.chunk.js"
+  },
+  {
+    "revision": "7d3fc6967da176131ca4",
+    "url": "/static/js/runtime-main.6e13ba53.js"
+  },
+  {
+    "revision": "28097faf00ae52120e12a782844dd5ff",
+    "url": "/static/media/game-background.28097faf.png"
+  }
+]);
